239c10239f3febc41332432221882b5a7391fe8a
Manihas Netha N <119604785+mani0bne@users.noreply.github.com>
2023-11-08 21:41:50 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**Add all required URLS here */
-const API_URL = ""
-const IMG_PATH=""
-const SEARCH_API = ""
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH= "https://image.tmdb.org/t/p/w1280"
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query"
 
 
 /** Select main, form and search from the DOM*/
@@ -21,16 +21,57 @@ async function getMovies(url){
     const data = await res.json()
 
     console.log("data",data)
+    showMovies(data.results)
 }
 
 function showMovies(movies) {
-    
+    main.innerHTML = ""
+    movies.forEach((movie)=>{
+        const {title, poster_path, vote_average, overview} = movie
+
+        const movieE1 = document.createElement('div')
+        movieE1.classList.add('movie')
+
+        movieE1.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class = "movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate}">${vote_average}</span>
+        </div>
+        <div class = "overview">
+            <h3>Overview</h3>
+            ${overview}
+        </div>
+        `;
+        main.appendChild(movieE1)
+    })
 }
 
 function getClassByRate(vote) {
+    if(vote >= 8){
+        return 'green'
+    }
+    else if(vote >=5){
+        return 'orange'
+    }
+    else{
+        return 'green'
+    }
    
 }
 
 /** Add Event Listeners on form
  * Add functionality to search movies through api
  */
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== ''){
+        getMovies(SEARCH_API + searchTerm)
+        search.value = ''
+    }else{
+        window.location.reload()
+    }
+})
